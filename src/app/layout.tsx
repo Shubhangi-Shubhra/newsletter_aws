b65c8200713668c00e1269bcf47f68dd7cfcb426
Shubhangi <shubhangishubhra@gmail.com>
2024-09-22 21:05:14 +0530

    m

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "@/shared/styles/globals.css";
+import "../shared/styles/globals.css";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
